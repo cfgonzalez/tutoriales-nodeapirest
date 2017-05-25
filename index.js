8eecb31e33deb6ffbd76bf71eda18b8298ac52ee
cfg62@hotmail.com <cfgonzalez@gmail.com>
2017-05-25 20:53:41 -0300

    Inicio de conexion con mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,15 +11,44 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 //---------------------------------------------------------------------------
-//verbos
-app.get('/hola/:name', (req, res) => {
-    res.send({message: `Hola ${req.params.name}`});
+//rutas
+app.get('/api/product', (req,res) => {
+    res.send(200, {products: [] });
+});
+
+app.get('/api/product/:productId', (req,res) => {
+    
+});
+
+app.post('/api/product', (req,res) => {
+   console.log(req.body );
+   res.status(200).send({message: 'El producto se ha recibido'});
+});
+
+app.put('/api/product/:productId', (req,res) => {
+    
+});
+
+app.delete('/api/product:productId', (req,res) => {
+    
 });
 
 //---------------------------------------------------------------------------
 // Startup del server
-app.listen(port, () => 
-{
-    console.log(`API REST corriendo en http://localhost:${port}`);
+
+mongoose.connect('mongodb://localhost:27017/shop',(err, res)=>{
+    if(err) 
+    {
+        return console.log(`Error al conectar a la base de datos: ${err}`);
+    }
+    console.log('Conexion a la BD establecida');
+
+    //Listen del puerto 
+    app.listen(port, () => {
+        console.log(`API REST corriendo en http://localhost:${port}`);
+    });
+
 });
+
+
 
